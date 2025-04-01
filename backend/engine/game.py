ddc4a61ded2546eb0251590d33daf46d48dd3c36
Jack Quinlan <jack.quinlannn@gmail.com>
2025-03-31 23:06:11 -0400

    refactoring structure

--- backend/engine/game.py
+++
@@ -1,8 +0,0 @@
-from engine.board import Board
-from engine.move_generation import MoveGen
-
-
-class Game:
-  def __init__(self):
-    self.board = Board()
-    self.moves = MoveGen(board=self.board)
